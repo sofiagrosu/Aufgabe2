836743967ac1025793d603e74d1793ad0e6ca0aa
sofiagrosu <grosu.sofia@gmail.com>
2025-10-17 10:33:10 +0300

    Class NrArray-1

---
+++ src/NrArray.java
@@ -0,0 +1,22 @@
+import java.util.List;
+
+public class NrArray {
+    private List<Integer> numbers;
+    private int n;
+    public NrArray(List<Integer> numbers, int n){
+        this.numbers = numbers;
+        this.n = n;
+    }
+    public int maxNumber(){
+        if(numbers.isEmpty()){
+            return 0;
+        }
+        int max = numbers.get(0);
+        for(int number : numbers){
+            if(number > max){
+                max = number;
+            }
+        }
+        return max;
+    }
+}
